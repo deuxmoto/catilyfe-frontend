488684568953e338202690df859589c58ef93385
Justin Niles <justin.niles12@gmail.com>
2018-01-21 01:01:53 -0800

    Fix clone typo

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -6,11 +6,16 @@ const fs = require("fs");
 const deployFolder = "./cl-deploy";
 const deployRepo = "git@github.com:deuxmoto/catilyfe-frontend-deployment.git";
 
-function executeCommand(command) {
-    return cp.spawnSync(command, {
+function executeCommand(command, throwOnFailure=true) {
+    const result = cp.spawnSync(command, {
         stdio: "inherit",
         shell: true
     });
+    if (throwOnFailure && (result.status !== 0 || result.error)) {
+        throw result.stderr || result.error || result.output;
+    }
+
+    return result;
 }
 
 // Ensure we're running in Travis
@@ -28,24 +33,25 @@ console.log(`
 
 // Clone the frontend deploy repo
 if (!fs.existsSync(`${deployFolder}`)) {
-    console.log(chalk.bold(`The deploy folder '${deployFolder}' doesn't exist! Attempting to clone into the repo...`));
-    executeCommand(`git clone ${deployRepo} ${deployRepo}`);
+    console.log(chalk.blue(`The deploy folder '${deployFolder}' doesn't exist! Attempting to clone into the repo...`));
+    executeCommand(`git clone ${deployRepo} ${deployFolder}`);
 }
 
 // Remove all existing deployed files
-console.log(chalk.bold("\nRemoving existing deployment files..."));
+console.log(chalk.blue("\nRemoving existing deployment files..."));
 executeCommand(`cd ${deployFolder}`);
 executeCommand("git rm -r ./* --ignore-unmatch");
 executeCommand("cd ..");
 
 // Build and copy janx
-console.log(chalk.bold("\nBuilding project with production flag set..."));
+console.log(chalk.blue("\nBuilding project with production flag set..."));
 executeCommand(`${process.env.TRAVIS_BUILD_DIR}/node_modules/@angular/cli/bin/ng build --target production --output-path ./${deployFolder}`);
 
 // Add and commit to deployment repo
+console.log(chalk.blue("\nDeploying updated files..."));
 executeCommand(`cd ${deployFolder}`);
 executeCommand("git add .");
-const commitResult = executeCommand(`git commit -m "DEPLOY: ${process.env.TRAVIS_COMMIT_MESSAGE}`);
+const commitResult = executeCommand(`git commit -m "DEPLOY: ${process.env.TRAVIS_COMMIT_MESSAGE}`, false);
 if (commitResult.status === 0) {
     // Only execute if commit succeeded (if no files changed, then commit would fail)
     executeCommand("git push");
