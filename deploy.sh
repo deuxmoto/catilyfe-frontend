391d1126513c864d0cc6b0bf15baa0ebfac96433
Justin Niles <justin.niles12@gmail.com>
2018-01-20 19:53:08 -0800

    Print Travis diagnostics

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,16 @@ set -e # stop on errors
 deployFolder="./catilyfe-frontend-deployment"
 deployRepo="git@github.com:deuxmoto/catilyfe-frontend-deployment.git"
 
+# Ensure we're running in Travis
 if [ ! $TRAVIS ]; then
     echo "Deploy script needs to be run by Travis CI. Aborting."
     exit
 fi
 
+printf "===DIAGNOSTICS===\n  Branch: $TRAVIS_BRANCH\n  Commit message: $TRAVIS_COMMIT_MESSAGE\n  Event type: $TRAVIS_EVENT_TYPE\n\n"
+exit
+
+# Clone the frontend deployment folder
 if [ ! -d "$deployFolder" ]; then
     printf "The dist repo '$deployFolder' doesn't exist! Attempting to clone into it...\n"
     git clone "$deployRepo"
@@ -20,7 +25,7 @@ git rm -r ./* --ignore-unmatch
 cd ..
 printf "Finished removing existing deployment files.\n\n"
 
-# Build n copy janx
+# Build and copy janx
 ./node_modules/@angular/cli/bin/ng build --target production
 cp -r ./dist/* "$deployFolder"
 printf "Build finished and out files copied to deployment folder.\n"
