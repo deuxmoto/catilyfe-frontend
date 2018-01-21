b98de1f7ebba0914c4030bb2a91fd8bb1e121c82
Justin Niles <justin.niles12@gmail.com>
2018-01-21 00:40:35 -0800

    Fix environment variable references

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -13,19 +13,17 @@ function executeCommand(command) {
     });
 }
 
-console.log(process.env);
-
 // Ensure we're running in Travis
-if (!process.env.$TRAVIS) {
+if (!process.env.TRAVIS) {
     console.log(chalk.red("Deploy script needs to be run by Travis CI. Aborting."));
     process.exit(-1);
 }
 
 console.log(`
 ===TRAVIS DIAGNOSTICS===
-  Branch: ${process.env.$TRAVIS_BRANCH}
-  Commit message: ${process.env.$TRAVIS_COMMIT_MESSAGE}
-  Event type: ${process.env.$TRAVIS_EVENT_TYPE}
+  Branch: ${process.env.TRAVIS_BRANCH}
+  Commit message: ${process.env.TRAVIS_COMMIT_MESSAGE}
+  Event type: ${process.env.TRAVIS_EVENT_TYPE}
 `);
 
 // Clone the frontend deploy repo
@@ -42,12 +40,12 @@ executeCommand("cd ..");
 
 // Build and copy janx
 console.log(chalk.bold("\nBuilding project with production flag set..."));
-executeCommand(`${process.env.$TRAVIS_BUILD_DIR}/node_modules/@angular/cli/bin/ng build --target production --output-path ./${deployFolder}`);
+executeCommand(`${process.env.TRAVIS_BUILD_DIR}/node_modules/@angular/cli/bin/ng build --target production --output-path ./${deployFolder}`);
 
 // Add and commit to deployment repo
 executeCommand(`cd ${deployFolder}`);
 executeCommand("git add .");
-const commitResult = executeCommand(`git commit -m "DEPLOY: ${process.env.$TRAVIS_COMMIT_MESSAGE}`);
+const commitResult = executeCommand(`git commit -m "DEPLOY: ${process.env.TRAVIS_COMMIT_MESSAGE}`);
 if (commitResult.status === 0) {
     // Only execute if commit succeeded (if no files changed, then commit would fail)
     executeCommand("git push");
